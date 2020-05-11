af40857fde063306b2e8a06792223767b40f1157
Vladimir Fedotkin <vfedotkin@list.ru>
2020-05-11 14:53:30 +0300

    Initial commit

---
+++ src/main/java/ru/vladimir/training/TrainInApplication.java
@@ -0,0 +1,13 @@
+package ru.vladimir.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainInApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainInApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/vladimir/training/TrainInApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vladimir.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainInApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
